664e97284b45ff3dfe5d6f64b430025582b2af34
majaul1989 <majadul1989@gmail.com>
2017-08-05 14:20:35 +0600

    First commit

---
+++ slide-functions.php
@@ -0,0 +1,11 @@
+<?php 
+
+/*********** short the skript******/
+function slide_excerpt($num) {
+$limit = $num+1;
+$excerpt = explode(' ', get_the_excerpt(), $limit);
+array_pop($excerpt);
+$excerpt = implode(" ",$excerpt);
+echo $excerpt;
+}
+ ?>
