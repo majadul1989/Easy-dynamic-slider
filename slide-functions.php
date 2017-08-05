3d47620a045068c734c0b553a12c3573b47da594
osman <osmansorkar@gmail.com>
2017-08-05 15:17:56 +0600

    remove un-nesesory space to improve view

--- slide-functions.php
+++ slide-functions.php
@@ -1,11 +1,1 @@
-<?php 
-
-/*********** short the skript******/
-function slide_excerpt($num) {
-$limit = $num+1;
-$excerpt = explode(' ', get_the_excerpt(), $limit);
-array_pop($excerpt);
-$excerpt = implode(" ",$excerpt);
-echo $excerpt;
-}
- ?>
+<?php /*********** short the skript******/    function easydynamicslider_excerpt($num) {    $limit = $num+1;    $excerpt = explode(' ', get_the_excerpt(), $limit);    array_pop($excerpt);    $excerpt = implode(" ",$excerpt);    echo $excerpt;    } ?>
